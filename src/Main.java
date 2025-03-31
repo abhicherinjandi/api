375852b26ecdf51f0326efd7b88fea1a445c2aa0
Abhilash CV <abhilash,cv@gmail.com>
2025-03-31 17:57:50 +0530

    First Commit to Main

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = 10;
+        float b = 20.0F;
+        if ((float) a < b) {
+            System.out.println("a is less than b");
+        }
+
+        for (int i = 10; i < 20; ++i) {
+            System.out.println(i);
+        }
+        System.out.println("First Commit in Main");
+
+    }
+}
